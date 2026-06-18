1fe544a592256d52baae76c557e926e303112fae
luisfelipemarincano <felipe.marin@itxpertix.com>
2026-06-18 16:57:34 -0500

    adding isSubsequence implementation

--- src/main/java/com/interview/strings/IsSubsequence.java
+++ src/main/java/com/interview/strings/IsSubsequence.java
@@ -6,8 +6,27 @@ package com.interview.strings;
  */
 public class IsSubsequence {
 
+    public static void main(String[] args) {
+        IsSubsequence solution = new IsSubsequence();
+
+        System.out.println(solution.isSubsequence("abc", "ahbgdc")); // esperado: true
+        System.out.println(solution.isSubsequence("axc", "ahbgdc")); // esperado: false
+        System.out.println(solution.isSubsequence("", "ahbgdc"));    // esperado: true
+    }
+
     public boolean isSubsequence(String s, String t) {
-        // TODO: recorre t con un puntero en s
-        throw new UnsupportedOperationException("Implementa isSubsequence");
+        
+        if(s.isEmpty()){
+            return true;
+        }
+        char[] textS = s.toCharArray();
+        char[] textT = t.toCharArray();
+        int indiceEnS = 0;
+        for(int i = 0; i < textT.length; i++){
+            if(textS[indiceEnS]== textT[i]){
+                indiceEnS++;
+            }
+        }
+        return indiceEnS == s.length();
     }
 }
