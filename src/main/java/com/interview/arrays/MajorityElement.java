42cfefc8af45fab10b2cd5c607f080908e0409c6
luisfelipemarincano <felipe.marin@itxpertix.com>
2026-06-25 19:59:44 -0500

    Adding majorityElementImperative

--- src/main/java/com/interview/arrays/MajorityElement.java
+++ src/main/java/com/interview/arrays/MajorityElement.java
@@ -7,15 +7,31 @@ package com.interview.arrays;
 public class MajorityElement {
 
     public static void main(String[] args) {
-        // TODO: crea casos manuales para probar majorityElement cuando implementes la solucion.
+        int[] nums = {4,4,4,4, 4, 8};
+        MajorityElement majorityElement = new MajorityElement();
+        System.out.println("Imperative ->" + majorityElement.majorityElementImperative(nums));
     }
 
     public int majorityElement(int[] nums) {
-        throw new UnsupportedOperationException("Implementa majorityElement");
+        return majorityElementImperative(nums);
     }
 
     public int majorityElementImperative(int[] nums) {
-        throw new UnsupportedOperationException("Implementa majorityElementImperative");
+
+        int majorityValue = 0;
+        for(int i = 0; i < nums.length; i++){
+            majorityValue = 1;
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    majorityValue++;
+                }
+                if(majorityValue > (nums.length/2)){
+                    return nums[i];
+                }
+            }
+        }
+        return 0;
+        
     }
 
     public int majorityElementFunctional(int[] nums) {
