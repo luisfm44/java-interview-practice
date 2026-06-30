3ff158febbcbe8c994b885df2acecd2c5354850e
luisfelipemarincano <felipe.marin@itxpertix.com>
2026-06-30 13:19:46 -0500

    Adding MissingBumber imperative

--- src/main/java/com/interview/arrays/MissingNumber.java
+++ src/main/java/com/interview/arrays/MissingNumber.java
@@ -8,36 +8,46 @@ public class MissingNumber {
 
     public static void main(String[] args) {
         int[] nums = {2,1,5,3,4,6,7,9,10};
-        orderArray(nums);
+        MissingNumber missingNumber = new MissingNumber();
+        System.out.println(missingNumber.missingNumber(nums));
     }
 
     public int missingNumber(int[] nums) {
         
-        //primero debemos ordenar la lista
-        //despues iteramos la lista y verificamos si hay algún vacío 
-        // entre el numero anterior y el que estoy evaluando
-        throw new UnsupportedOperationException("Implementa missingNumber");
+       return missingNumberImperative(nums);
     }
 
     public int missingNumberImperative(int[] nums) {
-        throw new UnsupportedOperationException("Implementa missingNumberImperative");
+       int[] orderedNums = orderArray(nums);
+       
+       if (orderedNums[0] != 0) {
+            return 0;
+        } 
+       
+        for(int i = 0; i < orderedNums.length -1; i++){
+            if(orderedNums[i + 1] - orderedNums[i]  > 1){
+                return orderedNums[i] +1;
+            }
+        }
+        
+        return orderedNums.length;
     }
 
-    public int missingNumberFunctional(int[] nums) {
+    public static int missingNumberFunctional(int[] nums) {
         throw new UnsupportedOperationException("Implementa missingNumberFunctional");
     }
 
-    public static int[] orderArray(int[] nums){
+    private int[] orderArray(int[] nums){
         int temp = 0;
-        for(int i = 0; i < nums.length; i++){
-                if(i !=nums.length){
-                    if(nums[i]> nums[i+1]){
-                        temp = nums[i];
-                        nums[i] = nums[i+1];
-                        nums[i+1] = temp;
-                    }
-                } 
+        for (int i = 0; i < nums.length-1; i++) {           // pasadas
+            for (int j = i + 1; j < nums.length; j++) {   // comparaciones
+                if(nums[i]> nums[j]){
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
             }
+        }
         return nums;
     }
 }
