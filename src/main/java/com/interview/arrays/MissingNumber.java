39c76aac22f0be70a8fa0f4ba9f17883dd3a4e02
luisfelipemarincano <felipe.marin@itxpertix.com>
2026-06-30 10:01:56 -0500

    Adding new exrcises with arrays

--- src/main/java/com/interview/arrays/MissingNumber.java
+++ src/main/java/com/interview/arrays/MissingNumber.java
@@ -7,10 +7,15 @@ package com.interview.arrays;
 public class MissingNumber {
 
     public static void main(String[] args) {
-        // TODO: crea casos manuales para probar missingNumber cuando implementes la solucion.
+        int[] nums = {2,1,5,3,4,6,7,9,10};
+        orderArray(nums);
     }
 
     public int missingNumber(int[] nums) {
+        
+        //primero debemos ordenar la lista
+        //despues iteramos la lista y verificamos si hay algún vacío 
+        // entre el numero anterior y el que estoy evaluando
         throw new UnsupportedOperationException("Implementa missingNumber");
     }
 
@@ -20,5 +25,19 @@ public class MissingNumber {
 
     public int missingNumberFunctional(int[] nums) {
         throw new UnsupportedOperationException("Implementa missingNumberFunctional");
+    }
+
+    public static int[] orderArray(int[] nums){
+        int temp = 0;
+        for(int i = 0; i < nums.length; i++){
+                if(i !=nums.length){
+                    if(nums[i]> nums[i+1]){
+                        temp = nums[i];
+                        nums[i] = nums[i+1];
+                        nums[i+1] = temp;
+                    }
+                } 
+            }
+        return nums;
     }
 }
